6f0939b9a366ca4090d6c965ac748120018a66e3
shahidbilagi <shahidbilagi7@gmail.com>
2025-04-24 16:52:53 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -4,8 +4,14 @@ public class Add {
         int num1 = 10;
         int num2 = 5;
 
+        int add =num1+num2;
+        int sub =num1-num2;
+
         int mul= num1 * num2;
         int div = num1 / num2;
+
+        System.out.println("The addition is:" + add);
+        System.out.println("The subraction is:" + sub);
 
         System.out.println("The first number is: " + num1);
         System.out.println("The second number is: " + num2);
